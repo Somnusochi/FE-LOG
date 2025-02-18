feabcda8db77253afb1caaf33569ef4d4fa6b454
Somnusochi <somnusochi@gmail.com>
2025-02-18 19:11:12 +0800

    chore: init project

---
+++ client/src/pages/index/index.tsx
@@ -0,0 +1,11 @@
+import './index.module.scss';
+
+function index() {
+  return (
+    <>
+      <p>dskdlkfldfgf</p>
+    </>
+  );
+}
+
+export default index;
---
+++ client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    open: true, // 自动在浏览器中打开
+    host: true, // 允许外部访问
+    port: 3000, // 可选：指定端口号
+  },
+})
